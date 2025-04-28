a462c1286132e9a165965ccf6783d16711f1a586
erose3 <eli.rosenthal3@gmail.com>
2025-04-28 21:09:40 +0000

    hyperlinks RCAV

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,25 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def all_directors
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def youngest_director
+    render({ :template => "misc_templates/youngest_director"})
+  end
+
+  def eldest_director
+    render({ :template => "misc_templates/eldest_director"})
+  end
+
+  def movies
+    render({ :template => "misc_templates/movies"})
+  end
+
+  def actors
+    render({ :template => "misc_templates/actors"})
+  end
+
 end
