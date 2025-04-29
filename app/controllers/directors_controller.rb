6c554c3d19e90a1668b44733abb87a9a078f3e32
erose3 <eli.rosenthal3@gmail.com>
2025-04-29 01:03:32 +0000

    about page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,6 +13,11 @@ class DirectorsController < ApplicationController
   end
 
   def about
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => the_id })
+    @the_director = matching_records.at(0)
+
     render({ :template => "misc_templates/director_about"})
   end
 
